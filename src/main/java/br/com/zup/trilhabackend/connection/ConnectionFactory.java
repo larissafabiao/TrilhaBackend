156f1ddf712e0bae34e89c2ff5aba497a2484713
joaoalveszup <joao.alves@zup.com.br>
2020-05-21 18:15:11 -0300

    Resolvendo problemas do tomcat

--- src/main/java/br/com/zup/trilhabackend/connection/ConnectionFactory.java
+++ src/main/java/br/com/zup/trilhabackend/connection/ConnectionFactory.java
@@ -2,17 +2,27 @@ package br.com.zup.trilhabackend.connection;
 
 import java.io.*;
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 
+@WebServlet(urlPatterns = "/teste", name = "testeSer")
 public class ConnectionFactory extends HttpServlet {
-	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException {
+    
+	/**
+     * 
+     */
+    private static final long serialVersionUID = 3993201784881460345L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        
+        
 		response.setContentType("text/html");
 		PrintWriter out;
 		try {
 			out = response.getWriter();
 			out.println("<html><body>");
-			out.println("<h1>Hello Readers</h1>");
+			out.println("<h1>TESTE COMANDO ECLIPSE</h1>");
 			out.println("</body></html>");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
